3eba3c3ca1cb3d226be12dd98101a641a4fe59cc
EvgKB <inflames76@gmail.com>
2017-09-29 16:38:10 +0300

    Javafx fired because too many limitations and problems with it. For thumbnail creation used jcodec library.

--- src/com/example/model/VideoMetadataReaderImpl.java
+++ src/com/example/model/VideoMetadataReaderImpl.java
@@ -1,38 +1,31 @@
 package com.example.model;
 
 import java.io.File;
-import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-import javafx.embed.swing.JFXPanel;
+import javafx.collections.MapChangeListener;
+import javafx.collections.ObservableMap;
 import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
 
 public class VideoMetadataReaderImpl implements MediaMetadataReader {
 	private Map<String, Set<String>> destination;
 	private Map<String, Map<String, String>> mmap = new HashMap<String, Map<String, String>>();
-	
-	//to avoid "Toolkit not initialized" exception
-	final JFXPanel fxPanel = new JFXPanel();
-	
+	ObservableMap<String, Object> metadata;
+			
 	public VideoMetadataReaderImpl(Map<String, Map<String, Set<String>>> reqMetadata) {
 		destination = reqMetadata.get("video");
 	}
 
 	public void extractMetadata(File file, MetadataRows mdataRows, JDBCUtilities util)
 			throws ClassNotFoundException, SQLException {
-		Media media = new Media(file.toURI().toString());
-		MediaPlayer mediaPlayer = new MediaPlayer(media);
-		mediaPlayer.setOnReady(new Runnable() {
-			public void run() {
-				System.out.println("extractMetadata MediaPlayer ready");
-				Map<String, Object> metadata = media.getMetadata();
-				System.out.println(metadata);
-			}
-		});
+//		Media media = new Media(file.toURI().toString());
+//		metadata = media.getMetadata();
+//		metadata.addListener((MapChangeListener<String, Object>) change -> {
+//		    System.out.println("MapChangeListener");
+//		});
 		
 	}
 	
--- src/com/example/model/VideoThumbCreatorImpl.java
+++ src/com/example/model/VideoThumbCreatorImpl.java
@@ -1,59 +1,41 @@
 package com.example.model;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
-import java.util.Map;
-
+import java.io.IOException;
 import javax.imageio.ImageIO;
-
-import javafx.collections.MapChangeListener;
-import javafx.collections.ObservableMap;
-import javafx.embed.swing.JFXPanel;
-import javafx.embed.swing.SwingFXUtils;
-import javafx.scene.image.WritableImage;
-import javafx.scene.media.Media;
-import javafx.scene.media.MediaPlayer;
-import javafx.scene.media.MediaView;
+import org.jcodec.api.FrameGrab;
+import org.jcodec.api.JCodecException;
+import org.jcodec.common.model.Picture;
+import org.jcodec.scale.AWTUtil;
 
 public class VideoThumbCreatorImpl implements MediaThumbnailCreator {
-	
-	//to avoid "Toolkit not initialized" exception
-	final JFXPanel fxPanel = new JFXPanel();
+		
 	
 	public String getThumbnail(File file, String relPath) {
 			
 		String videoFileName = FileUtils.ExctractFileName(file.getAbsolutePath());
 		String thumbFileName = FileUtils.FileNameWithoutExt(videoFileName)+"thmb.png";
 		String thumbPath = file.getAbsolutePath().replaceAll(videoFileName,	thumbFileName);
-				
-		Media media = new Media(file.toURI().toString());
-		ObservableMap<String, Object> metadata = media.getMetadata();
-		metadata.addListener((MapChangeListener<String, Object>) change -> {
-			System.out.println("MapChangeListener");
-		});
-		metadata = media.getMetadata();
-		MediaPlayer mediaPlayer = new MediaPlayer(media);
-		mediaPlayer.setOnReady(new Runnable() {
-			public void run() {
-				System.out.println("getThumbnail MediaPlayer ready");
-				int width = mediaPlayer.getMedia().getWidth();
-				int height = mediaPlayer.getMedia().getHeight();
-				WritableImage wim = new WritableImage(width, height);
-				MediaView mv = new MediaView();
-				mv.setFitWidth(width);
-				mv.setFitHeight(height);
-				mv.setMediaPlayer(mediaPlayer);
-				mv.snapshot(null, wim);
-				try {
-				    ImageIO.write(SwingFXUtils.fromFXImage(wim, null), "png", new File(thumbPath));
-				} catch (Exception s) {
-					s.printStackTrace();
-				    System.out.println(s);
-				} finally {
-					mediaPlayer.dispose();
-				}			
-			}
-		});
-						
+		
+		int frameNumber = 42;
+		Picture picture = null;
+		try {
+			picture = FrameGrab.getFrameFromFile(file, frameNumber);
+		} catch (IOException | JCodecException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		//for JDK (jcodec-javase)
+		BufferedImage bufferedImage = AWTUtil.toBufferedImage(picture);
+		try {
+			ImageIO.write(bufferedImage, "png", new File(thumbPath));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+								
 		return relPath.replaceAll(videoFileName, thumbFileName);
 	}
 
